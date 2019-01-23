0c8417259c62333f33e883f3bfe701d42e3d09c0
Cobyswan <Cobyswan@yahoo.com>
2019-01-22 17:02:55 -0700

    Full solution including black diamond

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,87 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
-
+import Post from './Post/Post';
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Search from './Header/Search/Search'
+
+
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
+      userSearch: ''
     };
 
     this.updatePost = this.updatePost.bind( this );
     this.deletePost = this.deletePost.bind( this );
     this.createPost = this.createPost.bind( this );
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
   
   componentDidMount() {
+    axios.get('https://practiceapi.devmountain.com/api/posts').then((response) => { 
+      this.setState({posts: response.data})
+    })
+  }
+
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text}).then((response) => {
+      this.setState({posts: response.data})
+    })
+  }
+
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`).then((response) => {
+      this.setState({posts: response.data})
+    })
+  }
+
+  createPost(text) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts`, {text}).then((response) => {
+      this.setState({posts: response.data})
+    })
+  }
+
+  searchPosts(text){
+    axios.get(`https://practiceapi.devmountain.com/api/posts/filter/?text=${text}`).then((response) => {
+      this.setState({posts: response.data})
+    })
+  }
+
+  handleInputChange(val){
+    this.searchPosts(val)
 
   }
 
-  updatePost() {
   
-  }
-
-  deletePost() {
-
-  }
-
-  createPost() {
-
-  }
-
   render() {
     const { posts } = this.state;
-
+    console.log(this.state.userSearch)
     return (
       <div className="App__parent">
-        <Header />
+        <Header handleInputChangeFn={this.handleInputChange}/>
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={this.createPost}/>
+          
+          {
+            posts.map((post) => {
+              return <Post 
+              key={post.id} 
+              text={post.text} 
+              date={post.date}
+              updatePostFn={this.updatePost}
+              id={post.id}
+              deletePostFn={this.deletePost}
+              />
+            })
+          }
           
         </section>
       </div>
--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -6,15 +6,36 @@ import SearchIcon from 'react-icons/lib/md/search';
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
+
 export default class Search extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      text: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this)
+  } 
+  
+  updateText(val){
+    this.setState({text: val})
+  }
+
+  handleInputChange(){
+    this.props.handleInputChangeFn(this.state.text)
+  }
+  
+  
   render() {
+    console.log(this.props)
+    console.log(this.state.text)
     return (
       <section className="Search__parent">
 
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input onChange={(e) => this.updateText(e.target.value)} placeholder="Search Your Feed" />
 
-          <SearchIcon id="Search__icon" />
+          <SearchIcon onClick={this.handleInputChange} id="Search__icon" />
         </div>
         
       </section>
